ac2548af8e004d6a9443d1163bcac1fb5faa8e19
suleman ahmed <ahmedsuleman@outlook.dk>
2020-06-07 13:49:55 +0200

    websocket changed in yarn.lock Simpel Header and some CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Header } from './components/header';
+
 import './App.css';
 
 function App() {
   return (
     <div >
-      hello world
+      <Header />
     </div>
   );
 }
