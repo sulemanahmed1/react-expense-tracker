6d98fc14d6f617670d035225b85cf515c177a096
suleman ahmed <ahmedsuleman@outlook.dk>
2020-06-07 21:22:37 +0200

    see expenses, green for income, red for expense, total balance added

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses} from './components/IncomeExpenses';
 import { TransactionList} from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider} from './context/GlobalState';
+
 import './App.css';
 
 function App() {
   return (
-    <div >
+    <GlobalProvider >
       <Header />
-      <div classname="container">
+      <div className="container">
         <Balance />
         <IncomeExpenses />
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
