c097ce6f65eccc362c51afe20b3d763327ce1c14
suleman ahmed <ahmedsuleman@outlook.dk>
2020-06-07 14:01:39 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Header } from './components/header';
-
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses} from './components/IncomeExpenses';
 import './App.css';
 
 function App() {
   return (
     <div >
       <Header />
+      <div classname="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
     </div>
   );
 }
